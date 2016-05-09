30b0e26a42b3c2acb04524444da70a1fc1ea1a05
Rory Vaughn <Rvaughn2525@gmail.com>
2016-05-09 18:00:20 -0500

    Player Movement
    Added player movement with controller and keyboard inputs.

---
+++ ADGP-115/Assets/Scripts/MoveCar.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCar : MonoBehaviour
+{
+    public float CarSpeed = 50.0f;
+    Vector3 position;
+
+
+    string movementAxisHorizontal;
+    string movementAxisVertical;
+    string fire;
+    public GameObject bullet;
+
+    // Use this for initialization
+    void Start () {
+        position = transform.position;
+        if (gameObject.tag == "P1")
+        {
+            movementAxisHorizontal = "P1Horizontal" ;
+            movementAxisVertical = "P1Vertical" ;
+            fire = "Fire1";
+        }
+        if (gameObject.tag == "P2")
+        {
+            movementAxisHorizontal = "P2Horizontal";
+            movementAxisVertical = "P2Vertical";
+            fire = "Fire2";
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    
+        position.x += Input.GetAxis (movementAxisHorizontal) * CarSpeed * Time.deltaTime;
+        position.z += Input.GetAxis(movementAxisVertical) * CarSpeed * Time.deltaTime;
+        transform.position = new Vector3(position.x, transform.position.y, position.z);
+        if (Input.GetButtonDown(fire))
+        {
+            Instantiate(bullet, new Vector3(position.x,position.y, position.z), Quaternion.identity);
+
+        }
+	}
+}
---
+++ ADGP-115/Assets/Scripts/bullet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet : MonoBehaviour {
+
+    float bulletSpeed = 70.0f;
+    GameObject Bullet;
+    Vector3 position;
+	// Use this for initialization
+	void Start () {
+        position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+    transform.position = new Vector3 (transform.position.x * Time.deltaTime * bulletSpeed, position.y, position.z);
+	}
+}
