4a45bb08fdbc108032f275cd417a7001e1bbdd1d
LoganPletcher <lpletch95@hotmail.com>
2016-05-10 12:23:16 -0500

    Edited bullets

--- ADGP-115/Assets/Scripts/MoveCar.cs
+++ ADGP-115/Assets/Scripts/MoveCar.cs
@@ -11,6 +11,7 @@ public class MoveCar : MonoBehaviour
     string movementAxisVertical;
     string fire;
     public GameObject bullet;
+    bool shooting = false;
 
     // Use this for initialization
     void Start () {
@@ -38,9 +39,12 @@ public class MoveCar : MonoBehaviour
         position.z += Input.GetAxis(movementAxisVertical) * CarSpeed * Time.deltaTime;
         transform.position = new Vector3(position.x, transform.position.y, position.z);
         if (Input.GetButtonDown(fire))
+            shooting = true;
+        if (Input.GetButtonUp(fire))
+            shooting = false;
+        if (shooting)
         {
             Instantiate(bullet, new Vector3(position.x,position.y, position.z), Quaternion.identity);
-
         }
 	}
 }
--- ADGP-115/Assets/Scripts/bullet.cs
+++ ADGP-115/Assets/Scripts/bullet.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class bullet : MonoBehaviour {
 
     float bulletSpeed = 70.0f;
-    GameObject Bullet;
+    float lifespan = .8f;
+    public GameObject Bullet;
     Vector3 position;
 	// Use this for initialization
 	void Start () {
@@ -13,7 +14,9 @@ public class bullet : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-    transform.position = new Vector3 (transform.position.x * Time.deltaTime * bulletSpeed, position.y, position.z);
+        transform.position = new Vector3 (transform.position.x + (Time.deltaTime * bulletSpeed), position.y, position.z);
+        lifespan -= Time.deltaTime;
+        if (lifespan <= 0)
+            Destroy(Bullet);
 	}
 }
