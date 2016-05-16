8f5a72398d09d4c0e927d108dac534770efdfc87
LoganPletcher <lpletch95@hotmail.com>
2016-05-16 16:39:20 -0500

    Scene with both pause script and weapon selection script implemented into the same canvas.

--- ADGP-115/Assets/Scripts/Weapon_Selection.cs
+++ ADGP-115/Assets/Scripts/Weapon_Selection.cs
@@ -9,6 +9,8 @@ public class Weapon_Selection : MonoBehaviour {
 
     public int p2Barrel = 2;
 
+    public GameObject MenuCamera;
+
     // Use this for initialization
     void Start () {
 	
@@ -68,5 +70,9 @@ public class Weapon_Selection : MonoBehaviour {
         Player2.GetComponent<MoveCar>().barrel = p2Barrel;
         Player2.tag = "P2";
         this.GetComponentInParent<Transform>().gameObject.SetActive(false);
+        if (MenuCamera != null)
+        {
+            MenuCamera.SetActive(false);
+        }
     }
 }
