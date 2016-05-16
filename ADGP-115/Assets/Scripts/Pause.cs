ded4efd7ef35e76f6a20f7caaf98374c0e9fa9ce
Guy Goudeau <guy.goudeau@students.aie.edu.au>
2016-05-16 15:36:41 -0500

    working on pause menu

---
+++ ADGP-115/Assets/Scripts/Pause.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour {
+
+    public Transform pauseMenu;
+    public GameObject audioSource;
+    bool soundToggle = true;
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void Mute()
+    {
+        soundToggle = !soundToggle;
+        if (soundToggle)
+        {
+            audioSource.SetActive(true);
+        }
+        else
+        {
+            audioSource.SetActive(false);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                pauseMenu.gameObject.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1;
+                pauseMenu.gameObject.SetActive(false);
+            }
+        }
+    }
+}
