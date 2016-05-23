fdfe2934a632ac63615a5c5b133182a8fec77829
Rory Vaughn <Rvaughn2525@gmail.com>
2016-05-23 11:52:17 -0500

    powerup fix

--- ADGP-115/Assets/Scripts/MoveCar.cs
+++ ADGP-115/Assets/Scripts/MoveCar.cs
@@ -8,6 +8,7 @@ public class MoveCar : MonoBehaviour
     Vector3 position;
 
     public float Health = 100;
+    public int GasTank = 1;
     string movementAxisHorizontal;
     string movementAxisVertical;
     string fire;
@@ -22,6 +23,7 @@ public class MoveCar : MonoBehaviour
     public bool BoostCurrent = false;
     public GameObject currentBullet;
     bool alive = true;
+    string fast;
 
     public Slider healthSlider;
     public Text currentWeapon;
@@ -34,12 +36,14 @@ public class MoveCar : MonoBehaviour
             movementAxisHorizontal = "P1Horizontal" ;
             movementAxisVertical = "P1Vertical" ;
             fire = "Fire1";
+            fast = "Fast";
         }
         if (gameObject.tag == "P2")
         {
             movementAxisHorizontal = "P2Horizontal";
             movementAxisVertical = "P2Vertical";
             fire = "Fire2";
+            fast = "Fast2";
         }
 
         if (barrel == 0)
@@ -78,6 +82,14 @@ public class MoveCar : MonoBehaviour
 	void Update () {
         if (alive)
         {
+            if (GasTank == 1)
+            {
+                if (Input.GetButtonDown(fast))
+                {
+                    CarSpeed = 150;
+                    GasTank = 1;
+                }
+            }
             if (CarSpeed == 150)
             {
                 Boost = 5;
@@ -120,6 +132,8 @@ public class MoveCar : MonoBehaviour
         healthSlider.value = Health;
         TankDeath();
         Delay -= Time.deltaTime;
+        if (GasTank == 2)
+            GasTank = 1;
 	}
 
     void TankDeath()
