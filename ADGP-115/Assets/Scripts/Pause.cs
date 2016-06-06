987942769daef08ad80087a2b3a33996fe1fde0e
Guy Goudeau <guy.goudeau@students.aie.edu.au>
2016-06-06 12:27:31 -0500

    commented out level manager and pause script

--- ADGP-115/Assets/Scripts/Pause.cs
+++ ADGP-115/Assets/Scripts/Pause.cs
@@ -4,55 +4,55 @@ using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour {
 
-    public Transform pauseMenu, playerUI;
-    public GameObject audioSource;
-    bool soundToggle = true;
+    public Transform pauseMenu, playerUI; // public transforms for the pause canvas and the health bars and weapon text
+    public GameObject audioSource; // pulic audio source, aka background music
+    bool soundToggle = true; // bool to determine if sound should be playing
 
-    public void LoadScene(string name)
+    public void LoadScene(string name) // function attached to Main Menu button to return to menu, can choose which scene to load based on name 
     {
-        SceneManager.LoadScene(name);
-        Time.timeScale = 1;
+        SceneManager.LoadScene(name); // loads scene based on scene's name
+        Time.timeScale = 1; // turns timescale back to 1 since it was set to 0 when paused
     }
 
-    public void Mute()
+    public void Mute() // function attached to mute button 
     {
-        soundToggle = !soundToggle;
-        if (soundToggle)
+        soundToggle = !soundToggle; // soundtoggle equals notSoundToggle
+        if (soundToggle) // if sound toggle is true
         {
-            audioSource.SetActive(true);
+            audioSource.SetActive(true); // set the audio source to inactive
         }
-        else
+        else // otherwise
         {
-            audioSource.SetActive(false);
+            audioSource.SetActive(false); // it's active
         }
     }
 
-    public void QuitGame()
+    public void QuitGame() // function attached to the Quit Game button
     {
-        Application.Quit();
+        Application.Quit(); // quits the application if it's an executable version
     }
 
-    void Update ()
+    void Update () // this gets updated every frame
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.P)) // if the p key is pressed,
         {
-            if (Time.timeScale == 1)
+            if (Time.timeScale == 1) // while timescale is currently 1, this would pause game
             {
-                Time.timeScale = 0;
-                pauseMenu.gameObject.SetActive(true);
-                playerUI.gameObject.SetActive(false);
+                Time.timeScale = 0; // set timescale to 0
+                pauseMenu.gameObject.SetActive(true); // turn on the pause canvas
+                playerUI.gameObject.SetActive(false); // turn off the player's UI
 
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None; // unlock the mouse cursor from the screen
+                Cursor.visible = true; // set cursor to be visible
             }
-            else
+            else // otherwise, if timescale was 0, this resumes game
             {
-                Time.timeScale = 1;
-                pauseMenu.gameObject.SetActive(false);
-                playerUI.gameObject.SetActive(true);
+                Time.timeScale = 1; // set timescale to 1
+                pauseMenu.gameObject.SetActive(false); // turn the pause canvas off
+                playerUI.gameObject.SetActive(true); // turn the player's UI back on
               
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked; // lock the cursor to the screen
+                Cursor.visible = false; // hide the cursor
             }
         }
     }
