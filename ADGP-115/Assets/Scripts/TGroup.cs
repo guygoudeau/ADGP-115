0efd8ab8439e43c9e67e317731f02da708d8dbb3
Rory Vaughn <Rvaughn2525@gmail.com>
2016-06-08 15:05:47 -0500

    penguins and toggle group work

--- ADGP-115/Assets/Scripts/TGroup.cs
+++ ADGP-115/Assets/Scripts/TGroup.cs
@@ -7,9 +7,10 @@ using UnityEngine.Events;
 
 public class TGroup : MonoBehaviour {
 
-    
-    
-    
+    Toggle T1;
+    Toggle T2;
+    Toggle T3;
+    Toggle T4;
     
     // Use this for initialization
     void Start () {
